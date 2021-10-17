b2ce1cc0e6062fa611222d26be9c0f6da95b60b2
Simon K <simon.kundrat@gmail.com>
2021-10-17 18:13:06 +0200

    App now using ECS

--- ecs.js
+++ ecs.js
@@ -23,9 +23,10 @@ function ECS() {
     this.addEntities = (entities) => entities.forEach(e => this.entities.push(e))
     this.step = () => {
         for (var system of this.systems) {
-            var system_componentnames = system.hookComponents.map(comp => comp.name)
+            var systemComponentNames = system.hookComponents.map(comp => comp.name)
             for (var entity of this.entities) {
-                var match = Object.keys(entity.components).every(comp => system_componentnames.includes(comp))
+                var componentNames = Object.keys(entity.components)
+                var match = systemComponentNames.every(name => componentNames.includes(name))
                 if (match) {
                     system.step(entity)
                 }
@@ -33,22 +34,3 @@ function ECS() {
         }
     }
 }
-
-const PositionComponent = new Component("position", {x: 0, y: 0})
-const RenderableComponent = new Component("renderable", {visible: true})
-
-const Boid = new Entity([PositionComponent])
-const RenderSystem = new System([PositionComponent], () => {
-    console.log("Start")
-
-    return (entity) => {
-        console.log("Step", entity)
-    }
-})
-
-const ecs = new ECS()
-ecs.addSystems([RenderSystem])
-ecs.addEntities([Boid])
-
-ecs.step()
-ecs.step()
--- main.js
+++ main.js
@@ -1,20 +1,39 @@
-let canvas = document.getElementById("canvas")
-let ctx = canvas.getContext("2d")
+const PositionComponent = new Component("position", {x: 0, y: 0})
+const RenderableComponent = new Component("renderable", {visible: true})
 
-const dpr = window.devicePixelRatio
-if (dpr > 1) {
-  let width = canvas.width
-  let height = canvas.height
+const ecs = new ECS()
+const RenderSystem = new System([PositionComponent], () => {
+  let canvas = document.getElementById("canvas")
+  let ctx = canvas.getContext("2d")
 
-  canvas.width = width * dpr
-  canvas.height = height * dpr
-  canvas.style.width = width + "px"
-  canvas.style.height = height + "px"
-  
-  ctx.scale(dpr, dpr)
-}
+  const dpr = window.devicePixelRatio
+  if (dpr > 1) {
+    let width = canvas.width
+    let height = canvas.height
 
-ctx.moveTo(0, 0)
-ctx.lineTo(400, 400)
-ctx.stroke()
+    canvas.width = width * dpr
+    canvas.height = height * dpr
+    canvas.style.width = width + "px"
+    canvas.style.height = height + "px"
+    
+    ctx.scale(dpr, dpr)
+  }
 
+  return (entity) => {
+    if (entity.components.renderable) {
+      let pos = entity.components.position
+      ctx.moveTo(pos.x, pos.y)
+      ctx.lineTo(400, 400)
+      ctx.stroke()
+    }
+  }
+})
+
+const ExampleLine = new Entity([PositionComponent, RenderableComponent])
+ExampleLine.components.position.x = 10
+ExampleLine.components.position.y = 10
+
+ecs.addSystems([RenderSystem])
+ecs.addEntities([ExampleLine])
+
+ecs.step()
