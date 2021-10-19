6d7a6630c5c91f5d172211eca0fe7d63cba00e14
Simon K <simon.kundrat@gmail.com>
2021-10-19 20:50:25 +0200

    Rotation wip

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-const PositionComponent = new Component("position", {x: 0, y: 0})
+const PositionComponent = new Component("position", {x: 0, y: 0, rot: 0})
 const RenderableComponent = new Component("renderable", {visible: true})
+
+const deg2rad = (angle) => angle * 3.1415926 / 180
+const rotate = (point) => {
+  return {
+    x: Math.cos(deg2rad(point.rot)) - Math.sin(deg2rad(point.rot)) + point.x,
+    y: Math.sin(deg2rad(point.rot)) + Math.cos(deg2rad(point.rot)) + point.y
+  }
+}
 
 const ecs = new ECS()
 const RenderSystem = new System([PositionComponent], () => {
@@ -22,8 +30,12 @@ const RenderSystem = new System([PositionComponent], () => {
   return (entity) => {
     if (entity.components.renderable) {
       let pos = entity.components.position
-      ctx.moveTo(pos.x, pos.y)
-      ctx.lineTo(400, 400)
+      console.log(pos)
+      let a = rotate(pos)
+      let b = rotate({x: a.x, y: a.y + 40, rot: pos.rot})
+      console.log(b)
+      ctx.moveTo(a.x, a.y)
+      ctx.lineTo(b.x, b.y)
       ctx.stroke()
     }
   }
@@ -32,6 +44,7 @@ const RenderSystem = new System([PositionComponent], () => {
 const ExampleLine = new Entity([PositionComponent, RenderableComponent])
 ExampleLine.components.position.x = 10
 ExampleLine.components.position.y = 10
+ExampleLine.components.position.rot = 45
 
 ecs.addSystems([RenderSystem])
 ecs.addEntities([ExampleLine])
