d921136974f48b6c74009166641bfae861a2beff
Krystex <5840423+Krystex@users.noreply.github.com>
2022-08-27 14:02:27 +0200

    4 to 2 spaces

--- ecs.js
+++ ecs.js
@@ -1,36 +1,36 @@
 function Component(name, initialState) {
-    this.new = () => {return initialState}
-    this.name = name
-    return this
+  this.new = () => { return initialState }
+  this.name = name
+  return this
 }
 function Entity(initialComponents) {
-    this.components = {}
-    for (let component of initialComponents) {
-        this.components[component.name] = component.new()
-    }
-    return this
+  this.components = {}
+  for (let component of initialComponents) {
+    this.components[component.name] = component.new()
+  }
+  return this
 }
 function System(hookComponents, start) {
-    this.hookComponents = hookComponents
-    this.start = start
-    this.step = start()
-    return this
+  this.hookComponents = hookComponents
+  this.start = start
+  this.step = start()
+  return this
 }
 function ECS() {
-    this.systems = []
-    this.entities = []
-    this.addSystems = (systems) => systems.forEach(s => this.systems.push(s))
-    this.addEntities = (entities) => entities.forEach(e => this.entities.push(e))
-    this.step = () => {
-        for (var system of this.systems) {
-            var systemComponentNames = system.hookComponents.map(comp => comp.name)
-            for (var entity of this.entities) {
-                var componentNames = Object.keys(entity.components)
-                var match = systemComponentNames.every(name => componentNames.includes(name))
-                if (match) {
-                    system.step(entity)
-                }
-            }
+  this.systems = []
+  this.entities = []
+  this.addSystems = (systems) => systems.forEach(s => this.systems.push(s))
+  this.addEntities = (entities) => entities.forEach(e => this.entities.push(e))
+  this.step = () => {
+    for (var system of this.systems) {
+      var systemComponentNames = system.hookComponents.map(comp => comp.name)
+      for (var entity of this.entities) {
+        var componentNames = Object.keys(entity.components)
+        var match = systemComponentNames.every(name => componentNames.includes(name))
+        if (match) {
+          system.step(entity)
         }
+      }
     }
+  }
 }
