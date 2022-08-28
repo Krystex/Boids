f01c2701c220ca112fcc91f9425d266049620caa
Krystex <5840423+Krystex@users.noreply.github.com>
2022-08-28 18:04:27 +0200

    Added some linear algebra (vec2)

--- ecs.js
+++ ecs.js
@@ -1,3 +1,43 @@
+/**
+ * 
+ * @param {float} angle angle in degrees
+ * @returns {float} angle in radians
+ */
+const deg2rad = (angle) => angle * 3.1415926 / 180
+
+class Vec2 {
+  /**
+   * New 2d vector with x and y component
+   * @param {float} x 
+   * @param {float} y 
+   */
+  constructor(x=0, y=0) {
+    this.x = x; this.y = y
+  }
+  /**
+   * Vector addition
+   * @param {Vec2} other 
+   * @returns {Vec2}
+   */
+  plus(other) {
+    return new Vec2(this.x + other.x, this.y + other.y)
+  }
+
+  magnitude() {
+    return Math.sqrt(this.x * this.x + this.y * this.y)
+  }
+
+  rotate_deg(angle) {
+    const magnitude = this.magnitude()
+    return new Vec2(
+      // Math.cos(deg2rad(angle)) - Math.sin(deg2rad(angle)) * magnitude,
+      // Math.sin(deg2rad(angle)) + Math.cos(deg2rad(angle)) * magnitude
+      Math.cos(deg2rad(angle)) * magnitude,
+      Math.sin(deg2rad(angle)) * magnitude
+    )
+  }
+}
+
 function Component(name, initialState) {
   this.new = () => { return initialState }
   this.name = name
