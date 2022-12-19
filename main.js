cd494fab1fe77ea274aa2f4e90b11ff1f80fc222
Krystex <5840423+Krystex@users.noreply.github.com>
2022-12-19 12:07:24 +0100

    Added velocity matching, centering, speed limit
    but seperation doesn't really work yet

--- main.js
+++ main.js
@@ -78,9 +78,9 @@ class RunECSSystem extends System {
     tickbutton.onclick = () => {
       if (!ecs.running) {
         ecs.startTime = new Date().getTime() - 32
-      ecs.tick()
+        ecs.tick()
+      }
     }
-  }
   }
   beforeTick() {}
   onEntity(_) {}
@@ -103,12 +103,30 @@ class BoidSystem extends System {
   }
   onEntity(_, entity) {
     // Calculate near boids
-    const maxDistance = 20.
-    let nearBoids = []
-    for (const [oEntityId, distance] of Object.entries(this.distanceMap[entity.id])) {
-      if (distance < maxDistance) {
-        nearBoids.push(ecs.entities[oEntityId])
+    const calculateNearBoids = (maxDistance) => {
+      let list = []
+      for (const [oEntityId, distance] of Object.entries(this.distanceMap[entity.id])) {
+        if (distance < maxDistance) {
+          list.push(ecs.entities[oEntityId])
+        }
       }
+      return list
+    }
+    const nearBoids = calculateNearBoids(20.)
+    const nearBoidsCentering = calculateNearBoids(40.)
+
+    // 0. Avoid borders
+    const margin = 50
+    const nudgeFactor = 1.3
+    const world = entity.components.position
+    if (world.pos.x < margin) {
+      world.vel.x += nudgeFactor
+    } else if (world.pos.x > (400 - margin)) {
+      world.vel.x -= nudgeFactor
+    } else if (world.pos.y < margin) {
+      world.vel.y += nudgeFactor
+    } else if (world.pos.y > (400 - margin)) {
+      world.vel.y -= nudgeFactor
     }
     
     // 1. Seperation
@@ -138,6 +156,32 @@ class BoidSystem extends System {
       entity.components.position.vel = entity.components.position.vel.add(force)
       entity.components.position.vel = entity.components.position.vel.unit().mul(oldMagnitude)
     }
+
+    /// 2. Try to match velocity
+    const velocityFactor = 0.1
+    // console.log("sumVel", sumVelocity)
+    if (nearBoids.length > 1) {
+      const sumVelocity = nearBoids
+        .map(b => b.components.position.vel)
+        .reduce((acc, vel) => acc.add(vel), new Vec2(0,0))
+      const avgVelocity = sumVelocity.div(nearBoids.length).mul(velocityFactor) // sumVelocity / nearBoids.length * velocityFactor
+      // console.log("avgVel", avgVelocity)
+      world.vel = world.vel.add(avgVelocity)
+    }
+
+    /// 3. Centering
+    const centeringFactor = 1.
+    if (nearBoidsCentering.length > 2) {
+      const sumPosition = nearBoidsCentering
+        .reduce((acc, e) => acc.add(e.components.position.pos), new Vec2(0,0))
+      const avgPos = sumPosition.div(nearBoidsCentering.length)
+      const direction = world.pos.sub(avgPos).mul(centeringFactor)
+      world.vel = world.vel.sub(direction)
+    }
+
+    /// Limit speed
+    const speedLimit = 25.
+    world.vel = world.vel.unit().mul(speedLimit)
   }
 }
 
@@ -146,7 +190,7 @@ ecs.addSystems([PhysicsSystem, CanvasRenderSystem, BoidSystem, RunECSSystem])
 for (let i=0; i<30; i++) {
   let boid = new Entity([PositionComponent, RenderableComponent, LineDrawableComponent])
   boid.components.position.pos = new Vec2(Math.random() * 400, Math.random() * 400)
-  boid.components.position.vel = new Vec2(Math.random_between(-30, 30), Math.random_between(-30, 30))
+  boid.components.position.vel = new Vec2(Math.random_between(-30, 30), Math.random_between(-30, 30)).unit().mul(3)
   ecs.addEntities([boid])
 }
 
